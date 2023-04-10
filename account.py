3d52bd43a881d947dab4b42785c63da5e616e0cc
Clay2M <clqystalcup@gmail.com>
2023-04-10 17:43:07 -0500

    Added DocStrings and Type Hinting to "account.py". Created unit tests using "pytest" for "account.py".

--- account.py
+++ account.py
@@ -1,25 +1,50 @@
 class Account:
+    """
+    A class representing details for an account object
+    """
 
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
+    def __init__(self, name: str) -> None:
+        """
+        Constructor to create initial state of an account object.
+        :param name: Account holder's name.
+        """
+        self.__account_name: str = name
+        self.__account_balance: float = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Method to add money to an account's balance.
+        :param amount: Amount to be added.
+        :return: Method's status. Returns True if successful.
+        """
         if amount <= 0:
             return False
         else:
             self.__account_balance += amount
             return True
 
-    def withdraw(self, amount):
-        if amount <= 0 or amount < self.__account_balance:
+    def withdraw(self, amount: float) -> bool:
+        """
+        Method to subtract money from an account's balance.
+        :param amount: Amount to be subtracted.
+        :return: Method's status. Returns True if successful.
+        """
+        if amount <= 0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Method to access an account's balance.
+        :return: Account's balance.
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Method to access an account holder's name.
+        :return: Account holder's name.
+        """
         return self.__account_name
---
+++ test_account.py
@@ -0,0 +1,34 @@
+import pytest
+from account import *
+
+
+class Test:
+    def setup_method(self):
+        self.test_account = Account('John')
+
+    def teardown_method(self):
+        del self.test_account
+
+    def test_init(self):
+        assert self.test_account.get_name() == 'John'
+        assert self.test_account.get_balance() == 0
+
+    def test_deposit(self):
+        assert self.test_account.deposit(0) is False
+        assert self.test_account.deposit(-38) is False
+        assert self.test_account.deposit(5) is True
+        assert self.test_account.get_balance() == pytest.approx(5, abs=0.001)
+        assert self.test_account.deposit(10.38) is True
+        assert self.test_account.get_balance() == pytest.approx(15.38, abs=0.001)
+
+    def test_withdraw(self):
+        self.test_account.deposit(50)
+        assert self.test_account.withdraw(0) is False
+        assert self.test_account.withdraw(-92) is False
+        assert self.test_account.get_balance() == pytest.approx(50, abs=0.001)
+        assert self.test_account.withdraw(65) is False
+        assert self.test_account.get_balance() == pytest.approx(50, abs=0.001)
+        assert self.test_account.withdraw(45) is True
+        assert self.test_account.get_balance() == pytest.approx(5, abs=0.001)
+        assert self.test_account.withdraw(4.98) is True
+        assert self.test_account.get_balance() == pytest.approx(0.02, abs=0.001)
